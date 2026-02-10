1d0ae96ee691e45f7dfd6dd1e9b9aed72fdd1a14
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-02-10 15:24:59 +0530

    Added server.js for class work

--- Class work/server.js
+++ Class work/server.js
@@ -1,1 +1,19 @@
-const http=require("http");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const method = req.method;
+    const url = req.url;
+    console.log(method, url);
+
+    if (method === "GET" && url === "/") {
+        res.end("Hello World");
+    } else {
+        res.statusCode = 404;
+        res.end("Not Found");
+    }
+});
+
+server.listen(4000, () => {
+    console.log("Server listening on port 3000");
+});
+
