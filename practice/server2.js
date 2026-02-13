8d7efcab61b2db20dbaad45f0cfaab346d2064a5
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-02-13 12:14:56 +0530

    today code update

---
+++ practice/server2.js
@@ -0,0 +1,34 @@
+const user={
+    name:"Aryan Dixit",
+    Age:"17"
+}
+const http = require("http");
+const fs=require("fs");
+
+const server = http.createServer((req, res) => {
+    const method = req.method;
+    const url = new URL(req.url,`http://${req.headers.host}`);
+
+    if(method==="GET" && url.pathname==="/user"){
+        res.writeHead(200,{"content-type":"application/json"})
+        res.end(JSON.stringify(user))
+    
+    }
+    if(method=="POST" && url.pathname=="/user"){
+        let body="";
+        req.on("data",(chunk)=>{
+            body+=chunk;
+        })
+        req.on("end",()=>{
+            fs.writeFileSync("./file.txt",body,()=>{
+                res.writeHead(201,"DATA WRITTEN");
+            })
+        })
+    }
+
+
+});
+
+server.listen(4000, () => {
+    console.log("Server listening on port 5000");
+}); 
