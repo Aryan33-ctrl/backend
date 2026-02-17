a8d79ca32127379a90204ee515ad8579921b3580
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-02-17 15:09:24 +0530

    notes application

--- Assignment/Assignment-3/practice-project-1/notes-server/server.js
+++ Assignment/Assignment-3/practice-project-1/notes-server/server.js
@@ -1,10 +1,63 @@
-const http=require("http");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-const server=http.createServer((req,res)=>{
-    res.writeHead(200,{"content-type":"text/plain"});
-    res.end("server is listening");
+const filePath = path.join(__dirname, "notes.json");
 
-    server.listen(3000,()=>{console.log("server running on http daily host 3000")});
+const server = http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = url.pathname;
+    const method = req.method;
 
+    if (method === "GET" && pathname === "/") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("server responded");
+    }
 
-})
+    else if (method === "GET" && pathname === "/notes") {
+        fs.readFile(filePath, "utf-8", (err, data) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("cannot get data");
+            } else {
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(data);
+            }
+        });
+    }
+
+    else if (method === "POST" && pathname === "/notes") {
+        let body = "";
+
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        req.on("end", () => {
+            fs.readFile(filePath, "utf-8", (err, data) => {
+                let notes = [];
+
+                if (!err && data) {
+                    notes = JSON.parse(data);
+                }
+
+                const parsedBody = JSON.parse(body);
+                notes.push(parsedBody);
+
+                fs.writeFile(filePath, JSON.stringify(notes), (err) => {
+                    if (err) {
+                        res.writeHead(500, { "Content-Type": "text/plain" });
+                        res.end("error saving note");
+                    } else {
+                        res.writeHead(201, { "Content-Type": "text/plain" });
+                        res.end("done!");
+                    }
+                });
+            });
+        });
+    }
+});
+
+server.listen(5000, () => {
+    console.log("Server running on http://localhost:5000");
+});
